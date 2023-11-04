c0c25d7d738a7d22ac3ad224b34fd134e2a468af
Artem Blodov <mr.samy@rambler.ru>
2023-11-04 13:12:59 +0200

    abc method for API and request

--- src/API.py
+++ src/API.py
@@ -1,5 +1,30 @@
-from abc import ABC
+import os
+from abc import ABC, abstractmethod
 
 
 class AbstractAPI(ABC):
-    pass
+
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+
+class SuperJob(AbstractAPI):
+    api_key: str = os.getenv('SUPER_JOB_KEY').lstrip().rstrip()
+
+    # super_job = https://api.superjob.ru/2.0/vacancies/:params
+    # super_job = https://api.superjob.ru/2.0/vacancies/234234/
+
+    def get_vacancies(self):
+        pass
+
+
+class HeadHunt(AbstractAPI):
+
+    def get_vacancies(self):
+        pass
+
+
+a = SuperJob()
+print(a.api_key)
+# print(a.super_job)
