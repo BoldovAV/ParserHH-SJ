a51d1a8d088e811ba1bfb796e8e38da76bc74b6b
Artem Blodov <mr.samy@rambler.ru>
2023-11-04 17:05:12 +0200

    start SJ, add flake8

--- src/API.py
+++ src/API.py
@@ -5,27 +5,37 @@ from abc import ABC, abstractmethod
 class AbstractAPI(ABC):
 
     @abstractmethod
-    def get_vacancies(self):
+    def get_vacancies(self, keywords):
         pass
 
 
 class SuperJob(AbstractAPI):
     api_key: str = os.getenv('SUPER_JOB_KEY').lstrip().rstrip()
+    
+    def __init__(self):
+        self.url = 'https://api.superjob.ru/2.0/vacancies'
+        self.headers = {'X-Api-App-Id': self.api_key}
+        self.params = {
+            'count': 2,
+            'page': 1,
+            'town': 'Kaliningrad',
+        }
 
     # super_job = https://api.superjob.ru/2.0/vacancies/:params
     # super_job = https://api.superjob.ru/2.0/vacancies/234234/
 
-    def get_vacancies(self):
+    def get_vacancies(self, keywords):
         pass
 
 
 class HeadHunt(AbstractAPI):
     base_url = "https://api.hh.ru/"
 
-    def get_vacancies(self):
+    def get_vacancies(self, keywords):
         pass
 
 
 a = SuperJob()
 print(a.api_key)
+print(a.headers)
 # print(a.super_job)
